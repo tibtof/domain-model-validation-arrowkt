ccec1e0820a5f83dc280f72aa3657c2f53a2dec2
Tiberiu Tofan <tibtof@gmail.com>
2022-03-29 00:54:13 +0300

    run from gradle and readme

---
+++ readme.md
@@ -0,0 +1,7 @@
+## Domain Model Validation In Kotlin 
+Supporting project for the "Domain Model Validation In Kotlin" series on https://tibtof.medium.com/
+
+Running the application:
+```shell
+./gradlew run -q --console=plain
+```
--- src/main/kotlin/cli/SendEmailConsole.kt
+++ src/main/kotlin/cli/SendEmailConsole.kt
@@ -11,14 +11,16 @@ import domain.InterruptedError
 import domain.RuntimeError
 
 
-suspend fun readInput(): Either<ApplicationError, Tuple4<String, String, List<String>, List<String>>> = either {
-    Tuple4(
-        readString("from").bind(),
-        readString("to").bind(),
-        readString("cc").bind().asList(),
-        readString("bcc").bind().asList()
-    )
-}
+suspend fun readInput():
+        Either<ApplicationError, Tuple4<String, String, List<String>, List<String>>> =
+    either {
+        Tuple4(
+            readString("from").bind(),
+            readString("to").bind(),
+            readString("cc").bind().asList(),
+            readString("bcc").bind().asList()
+        )
+    }
 
 private fun readString(message: String): Either<ApplicationError, String> = try {
     println("$message (q to exit)?")
